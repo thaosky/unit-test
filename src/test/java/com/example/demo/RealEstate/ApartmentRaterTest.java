496ab0b1996bf5ca169942297c0559477e53ac7d
thaosky <thaohsk@gmail.com>
2021-09-26 16:56:40 +0700

    Write unit test for Apartment app

---
+++ src/test/java/com/example/demo/RealEstate/ApartmentRaterTest.java
@@ -0,0 +1,71 @@
+package com.example.demo.RealEstate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApartmentRaterTest {
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"72.0, 250000.0, 0", "48.0, 350000.0, 1", "30.0, 600000.0, 2"})
+    void should_ReturnCorrectRating_When_CorrectApartment(Double area, Double price, int rating) {
+        // given
+        Apartment apartment = new Apartment(area, new BigDecimal(price));
+        int expected = rating;
+
+        // when
+        int actual = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_ReturnErrorValue_When_IncorrectApartment() {
+        // given
+        Apartment apartment = new Apartment(0, new BigDecimal(350000.0));
+        int expected = -1;
+
+        // when
+        int actual = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_CalculateAverageRating_When_CorrectApartmentList() {
+        // given
+        List<Apartment> apartments = new ArrayList<>();
+        apartments.add(new Apartment(72.0, new BigDecimal(250000.0)));
+        apartments.add(new Apartment(48.0, new BigDecimal(350000.0)));
+        apartments.add(new Apartment(30.0, new BigDecimal(600000.0)));
+
+        double expected = 1.0;
+
+        // when
+        double actual = ApartmentRater.calculateAverageRating(apartments);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_ThrowExceptionInCalculateAverageRating_When_EmptyApartmentList() {
+        // given
+        List<Apartment> apartmentList = new ArrayList<>();
+        // when
+        Executable executable = () -> ApartmentRater.calculateAverageRating(apartmentList);
+        // then
+        assertThrows(RuntimeException.class, executable);
+    }
+
+}
